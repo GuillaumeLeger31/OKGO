c62b9218f36967e48ab7802c0ea28ac30a1c9cb5
Guillaume Leger <guillaumeleger140@gmail.com>
2022-02-17 20:42:53 +0100

    création API

---
+++ API/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+require('dotenv').config();
+mongoose
+  .connect(process.env.DB_URI,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true 
+    })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send('<h1>Bonjour</h1>')
+});
+
+
+module.exports = app;
